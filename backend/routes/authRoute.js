3d58f5c0e275e8aace98a32c31f3f008f0c2f4ef
Jaffer <mjaffer720@gmail.com>
2023-11-29 20:16:24 +0500

    PiazzaApi

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,10 @@
+import express  from "express";
+import { login, register} from "../controller/auth.js";
+import { loginValidation, registerValidation } from "../utils/validator.js";
+
+const router =express.Router();
+
+router.post("/register",registerValidation, register);
+router.post("/login",loginValidation, login);
+
+export default router
